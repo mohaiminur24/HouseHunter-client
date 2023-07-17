3e9da144418d4a66f31f5a688f83e66ea5776e9b
John Doe <mohaiminur24@gmail.com>
2023-07-18 02:09:51 +0600

    Use react hook from and password validation is complete

--- src/Components/UserLoginRegistration/LoginPage.jsx
+++ src/Components/UserLoginRegistration/LoginPage.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Container from "../ReUseableComponents/Container";
 import { NavLink } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const LoginPage = () => {
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
+    const handleLogin = data =>{
+        console.log(data)
+        reset();
+    }
   return (
     <Container>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5 justify-center items-center min-h-screen">
@@ -13,28 +19,32 @@ const LoginPage = () => {
         />
         <div className="p-10 border rounded-md shadow-sm font-Inter">
           <h1 className="text-2xl font-bold mb-5">Login User</h1>
-          <form>
+          <form onSubmit={handleSubmit(handleLogin)}>
             <div className="mb-2">
               <label className="text-sm" htmlFor="email">
                 Email Address
               </label>
               <input
+              {...register("email", { required: true })}
                 className="w-full border mt-1 text-gray-400 outline-none px-5 py-2 text-xs rounded-sm shadow-sm"
                 type="email"
                 name="email"
                 id="email"
               />
+              {errors.email?.type === 'required' && <p className="text-xs mt-1 text-red-500 font-Raleway" role="alert">Email is required</p>}
             </div>
             <div className="mb-5">
               <label className="text-sm" htmlFor="email">
                 Password
               </label>
               <input
+              {...register("password", { required: true })}
                 className="w-full border mt-1 text-gray-400 outline-none px-5 py-2 text-xs rounded-sm shadow-sm"
-                type="email"
-                name="email"
-                id="email"
+                type="password"
+                name="password"
+                id="password"
               />
+              {errors.password?.type === 'required' && <p className="text-xs mt-1 text-red-500 font-Raleway" role="alert">Password is required</p>}
             </div>
             <input
               className="w-full py-2 bg-yellow-500 rounded-sm font-semibold text-white tracking-wider cursor-pointer"
--- src/Components/UserLoginRegistration/RegistrationPage.jsx
+++ src/Components/UserLoginRegistration/RegistrationPage.jsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Container from "../ReUseableComponents/Container";
+import { useForm } from "react-hook-form";
 
 const RegistrationPage = () => {
+  const [passError, setErrorMessage] = useState(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const handleRegistration = (data) => {
+    // password validation is here
+    setErrorMessage(null);
+    const isContainsUppercase = /^(?=.*[A-Z]).*$/;
+    const isContainsSymbol =
+      /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).*$/;
+    if (data.password.length < 6) {
+      setErrorMessage("Password must have more then 6 letter");
+      return;
+    } else if (!isContainsUppercase.test(data.password)) {
+      setErrorMessage("Password must have a Capital letter");
+      return;
+    } else if (!isContainsSymbol.test(data.password)) {
+      setErrorMessage("Password must have a special caracter");
+      return;
+    }
+    // Password validation end from here
+
+    reset();
+  };
+
   return (
     <Container>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5 justify-center items-center min-h-screen">
@@ -13,59 +43,116 @@ const RegistrationPage = () => {
         />
         <div className="p-10 border rounded-md shadow-sm font-Inter">
           <h1 className="text-2xl font-bold mb-5">User Registration</h1>
-          <form>
+          <form onSubmit={handleSubmit(handleRegistration)}>
             <div className="mb-2">
               <label className="text-sm" htmlFor="email">
                 Name
               </label>
               <input
+                {...register("name", { required: true })}
                 className="w-full border mt-1 text-gray-400 outline-none px-5 py-2 text-xs rounded-sm shadow-sm"
                 type="text"
                 name="name"
                 id="name"
               />
+              {errors.name?.type === "required" && (
+                <p
+                  className="text-xs mt-1 text-red-500 font-Raleway"
+                  role="alert"
+                >
+                  Name is required
+                </p>
+              )}
             </div>
             <div className="mb-2">
               <label className="text-sm" htmlFor="email">
                 Email Address
               </label>
               <input
+                {...register("email", { required: true })}
                 className="w-full border mt-1 text-gray-400 outline-none px-5 py-2 text-xs rounded-sm shadow-sm"
                 type="email"
                 name="email"
                 id="email"
               />
+              {errors.email?.type === "required" && (
+                <p
+                  className="text-xs mt-1 text-red-500 font-Raleway"
+                  role="alert"
+                >
+                  Email is required
+                </p>
+              )}
             </div>
             <div className="mb-2">
               <label className="text-sm" htmlFor="email">
                 Password
               </label>
               <input
+                {...register("password", { required: true })}
                 className="w-full border mt-1 text-gray-400 outline-none px-5 py-2 text-xs rounded-sm shadow-sm"
-                type="email"
-                name="email"
-                id="email"
+                type="password"
+                name="password"
+                id="password"
               />
+              {errors.password?.type === "required" && (
+                <p
+                  className="text-xs mt-1 text-red-500 font-Raleway"
+                  role="alert"
+                >
+                  Password is required
+                </p>
+              )}
+              {passError && (
+                <p
+                  className="text-xs mt-1 text-red-500 font-Raleway"
+                  role="alert"
+                >
+                  {passError}
+                </p>
+              )}
             </div>
             <div className="mb-5">
               <label className="text-sm" htmlFor="email">
                 Phone
               </label>
               <input
+                {...register("phone", { required: true })}
                 className="w-full border mt-1 text-gray-400 outline-none px-5 py-2 text-xs rounded-sm shadow-sm"
                 type="tel"
                 name="phone"
                 id="phone"
               />
+              {errors.phone?.type === "required" && (
+                <p
+                  className="text-xs mt-1 text-red-500 font-Raleway"
+                  role="alert"
+                >
+                  Phone is required
+                </p>
+              )}
             </div>
             <div className="mb-5">
               <label className="text-sm mr-5" htmlFor="email">
                 User Role:
               </label>
-              <select name="role" className="text-sm border outline-none px-3 py-1 rounded-sm" id="role">
+              <select
+                {...register("role", { required: true })}
+                name="role"
+                className="text-sm border outline-none px-3 py-1 rounded-sm"
+                id="role"
+              >
                 <option value="HouseRenter">House Renter</option>
                 <option value="HouseOwner">House Owner</option>
               </select>
+              {errors.role?.type === "required" && (
+                <p
+                  className="text-xs mt-1 text-red-500 font-Raleway"
+                  role="alert"
+                >
+                  Role is required
+                </p>
+              )}
             </div>
             <input
               className="w-full py-2 bg-yellow-500 rounded-sm font-semibold text-white tracking-wider cursor-pointer"
@@ -73,7 +160,7 @@ const RegistrationPage = () => {
               value="Registration"
             />
             <div className="mt-5 text-sm">
-              <span>I  have an Account</span>{" "}
+              <span>I have an Account</span>{" "}
               <NavLink to="/login" className="text-yellow-500 font-bold">
                 Login Account
               </NavLink>
